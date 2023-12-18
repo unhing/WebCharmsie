87b2dc96401882089feefd7454eafe86b9a05edc
milkyway1106 <nganptt21411c@st.uel.edu.vn>
2023-12-18 13:42:30 +0700

    edit css in admin FE

--- AdminFE/src/app/app.component.ts
+++ AdminFE/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AdminFE';
+  isClickedOption1 = false;
+  isClickedOption2 = false;
+
+  toggleSubmenuOption1() {
+    this.isClickedOption1 = !this.isClickedOption1;
+  }
+
+  toggleSubmenuOption2() {
+    this.isClickedOption2 = !this.isClickedOption2;
+  }
 }
